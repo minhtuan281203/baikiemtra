56bb7a0b4b755d9ae3be273cad11666f517632c2
Minh Tuấn <khanhdo601@gmail.com>
2022-08-06 22:26:15 +0700

    exercise1
    kiemtra

---
+++ GeometryExample/bai1/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Cylinder cy = new Cylinder();
+        cy.Result();
+
+        cy.Process();
+
+        Console.ReadLine();
+    }
+}
